7ea1228a5aa29c7d7dcef5148ac1a7e84cb85883
Kenny Zhu <KennyZhu@MacBook-Pro-5.local>
2019-08-03 21:55:02 -0500

    initial checking

---
+++ HelloWorld/src/com/mn/kenny/Addition.java
@@ -0,0 +1,14 @@
+package com.mn.kenny;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println("start the program");
+		int a = 5;
+		int b = 8;
+		int sum = a + b;
+		System.out.println("the sum is:" + sum);
+		System.out.println("end the program");
+	}
+
+}
